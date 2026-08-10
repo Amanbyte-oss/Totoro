6c20c9322255b7c209da6b2deceb3c915f496c9e
Aman Sharma <amansharma882007@gmail.com>
2026-08-10 10:13:31 +0530

    1.0.1 update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
 		applicationId = "com.aman.vanish"
 		minSdk = 23
 		targetSdk = 37
-		versionCode = 1
-		versionName = "1.0.0"
+		versionCode = 2
+		versionName = "1.0.1"
 		testInstrumentationRunner = "com.aman.vanish.HiltTestRunner"
 		ksp {
 			arg("room.generateKotlin", "true")
